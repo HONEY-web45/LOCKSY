e9ea4fe6486f8e62b620abfc36900013076a89c5
Honey <nitik2643@gmail.com>
2025-06-29 17:38:23 +0530

    O

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   <Route path="/" element={<Home/>}/>
   <Route path="/about" element={<About/>}/>
  
-  <Route path="/pass" element={<PrivateRoute children={<Manager/>}/>}/>
+  <Route path="/pass" element={ <PrivateRoute><Manager/></PrivateRoute>}/>
   <Route path="/contact" element={<ContactPage/>}/>
   
   
