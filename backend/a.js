d2386c32680ddbf6cf53439c77efb2b5e7d18a93
Honey <nitik2643@gmail.com>
2025-06-29 14:21:32 +0530

    :P:

--- backend/a.js
+++ backend/a.js
@@ -67,6 +67,6 @@ const arr=await collection.find({email:email}).toArray()
 
 
 
-app.listen(port, () => {
+app.listen(port,'0.0.0.0', () => {
   console.log(`Example app listening on port ${port}`)
 })
