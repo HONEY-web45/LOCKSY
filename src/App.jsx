4d520df2f138e49813ac24831d0369f96b2518c4
Honey <nitik2643@gmail.com>
2025-08-17 15:12:46 +0530

    P

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   playsInline
   className="fixed top-0 left-0 w-full h-full object-cover z-[-1]"
 >
-  {/* <source src="b6.mp4" type="video/mp4" /> */}
+  <source src="b6.mp4" type="video/mp4" />
   Your browser does not support the video tag.
 </video>
 {/* <img src="b5.gif" alt="" className='fixed top-0 left-0 w-full h-full object-cover z-[-1]'  /> */}
